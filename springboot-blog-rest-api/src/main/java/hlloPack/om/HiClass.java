1193cb55b4d7a56d4d22ae3e32ed543f5f61a833
Prasad1918 <prasadrelangi18@gmail.com>
2021-12-13 16:41:22 +0530

    One more change

--- springboot-blog-rest-api/src/main/java/hlloPack/om/HiClass.java
+++ springboot-blog-rest-api/src/main/java/hlloPack/om/HiClass.java
@@ -1,5 +1,12 @@
 package hlloPack.om;
 
 public class HiClass {
+	
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+		String s="mrps Prasad";
+		String er="Realme Mobiles.com.in";
+	}
 
 }
